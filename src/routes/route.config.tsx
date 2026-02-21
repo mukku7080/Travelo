2da3604cdddf94f1e905d43c6aea5ab87646a4dc
Rishabh Singh <rishabhsingh12251992@gmail.com>
2026-02-21 11:45:00 +0530

    hotel listing page

--- src/routes/route.config.tsx
+++ src/routes/route.config.tsx
@@ -2,6 +2,7 @@ import HomePages from "@/pages/HomePages";
 import CreateAccount from "@/pages/CreateAccount";
 import MainLayout from "@/layouts/MainLayouts";
 import LoginPage from "@/pages/LoginPage";
+import HotelListingPage from "@/pages/HotelListingPage";
 
 export const routes = [
     {
@@ -11,6 +12,7 @@ export const routes = [
             { path: "/", element: <HomePages /> },
             { path: "/signup", element: <CreateAccount /> },
             { path: "/login", element: < LoginPage /> },
+            { path: "/page1", element: < HotelListingPage /> },
             //   { path: "/login", element: <Login /> },
         ],
     },
